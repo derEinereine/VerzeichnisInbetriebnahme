83a6591e442a0dbbb8422b9fcdaf398c741b8107
derEinereine <fabiangillich507@gmail.com>
2025-10-29 11:01:49 +0100

    Add files via upload

---
+++ Grundlagen der Physik 2025/HAUPTBETRIEB.bat
@@ -0,0 +1,8 @@
+echo off 
+
+ren 000_Vorwort N000_Vorwort
+ren 001_Arbeitszeitdokumentation N001_Arbeitszeitdokumentation
+ren 002_Bilder N002_Bilder
+ren 003_Anforderung N003_Anforderung
+ren 004_Arbeitsplatz N004_Arbeitsplatz
+ren 005_Hilfsmittel N005_Hilfsmittel
---
+++ Grundlagen der Physik 2025/INBETRIEBNAHME.bat
@@ -0,0 +1,8 @@
+echo off 
+
+ren N000_Vorwort 000_Vorwort
+ren N001_Arbeitszeitdokumentation 001_Arbeitszeitdokumentation
+ren N002_Bilder 002_Bilder
+ren N003_Anforderung 003_Anforderung
+ren N004_Arbeitsplatz 004_Arbeitsplatz
+ren N005_Hilfsmittel 005_Hilfsmittel
